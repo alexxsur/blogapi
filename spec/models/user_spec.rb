0bb4402698f454bbd4f2b2f02c96ea0a192ffb8b
Alex <alexxsur@gmail.com>
2023-11-09 14:33:45 -0500

    List and show Post : tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   describe "validations" do
     it "validate presence of required fields" do
       should validate_presence_of(:email)
@@ -12,6 +11,5 @@ RSpec.describe User, type: :model do
     it "validate relations" do
       should have_many(:posts)
     end
-
   end
 end
